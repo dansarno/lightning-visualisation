9de7f799b545c4882264d711c4aa3fe5edfca89d
Daniel Sarno <dansarno91@gmail.com>
2020-10-31 15:59:47 +0000

    Adding initial sketch setup

---
+++ node.js
@@ -0,0 +1,21 @@
+class Grid {
+
+}
+
+class Node {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+    this.status = status;
+  }
+
+  show() {
+
+  }
+
+}
---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(600, 400);
+  noStroke();
+  fill(20, 64, 190);
+}
+
+function draw() {
+  background(200);
+  ellipse(50, 50, 5);
+}
