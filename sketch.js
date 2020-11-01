6d9d92b3af7c792a6bfd9d3ec40021d370dcd330
Daniel Sarno <dansarno91@gmail.com>
2020-11-01 17:04:29 +0000

    Create grid on setup

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
+let grid;
+
 function setup() {
   createCanvas(600, 600);
-  noStroke();
-  fill(20, 64, 190);
+  grid = new Grid(50, 50, width, height);
 }
 
 function draw() {
   background(255);
-  let grid = new Grid(50, 50, width, height);
   grid.render();
   console.log(grid);
   noLoop();
