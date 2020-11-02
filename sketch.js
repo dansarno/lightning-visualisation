21c54aad93db191024156897d0719865fafadb01
Daniel Sarno <dansarno91@gmail.com>
2020-11-02 10:54:00 +0000

    Added more walls

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
   // frameRate(1);
   createCanvas(800, 800);
 
-  grid = new Grid(cols, rows, width, 600, 0.3);
+  grid = new Grid(cols, rows, width, 600, 0.4);
   grid.setStartNode(floor(random(0, cols)), 0);
   grid.setEndNode(floor(random(0, cols)), rows - 1);
   grid.initialise();
